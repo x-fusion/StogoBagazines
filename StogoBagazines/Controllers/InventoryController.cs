bf5c1633d21afbe9697b05598d42afec13961442
Edvardas-Arnoldas Alaburda <e.alaburda@lifosa.com>
2019-10-21 15:42:41 +0300

    api workground

--- StogoBagazines/Controllers/InventoryController.cs
+++ StogoBagazines/Controllers/InventoryController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using StogoBagazines.DataAccess;
@@ -10,23 +11,86 @@ using StogoBagazines.DataAccess.Objects;
 
 namespace StogoBagazines.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class InventoryController : ControllerBase
     {
-        private readonly ILogger<WeatherForecastController> logger;
+        /// <summary>
+        /// Logging object
+        /// </summary>
+        private readonly ILogger<InventoryController> logger;
+        /// <summary>
+        /// Database object
+        /// </summary>
         private readonly Database database;
-
-        public InventoryController(ILogger<WeatherForecastController> logger, Database database)
+        /// <summary>
+        /// Repository object
+        /// </summary>
+        private readonly InventoryBaseRepository repository;
+        /// <summary>
+        /// API endpoint constructor
+        /// </summary>
+        /// <param name="logger">Dependency injection logging object</param>
+        /// <param name="database">Dependency injection database object</param>
+        public InventoryController(ILogger<InventoryController> logger, Database database)
         {
             this.logger = logger;
             this.database = database;
+            repository = new InventoryBaseRepository(database);
+        }
+        // GET: api/Inventory
+        /// <summary>
+        /// Returns all available InventoryBase objects
+        /// </summary>
+        /// <returns>Enumerable array</returns>
+        [HttpGet]
+        public IEnumerable<InventoryBase> Get()
+        {
+            return repository.ReadAll();
         }
 
-        [HttpGet]
-        public IEnumerable<InventoryBase> Get(object id)
+        // GET: api/Inventory/5
+        /// <summary>
+        /// Returns particular Inventory based on identity key
+        /// </summary>
+        /// <param name="id">Identification key</param>
+        /// <returns>Inventory object</returns>
+        [HttpGet("{id}", Name = "Get")]
+        public InventoryBase Get(object id)
         {
-            return null;
+            return repository.Read(id);
+        }
+
+        // POST: api/Inventory
+        /// <summary>
+        /// Inserts new Inventory item into database set
+        /// </summary>
+        /// <param name="value">Inventory item serialised in request body</param>
+        [HttpPost]
+        public void Post([FromBody] InventoryBase value)
+        {
+
+        }
+
+        // PUT: api/Test/5
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
         }
     }
 }
---
+++ StogoBagazines/DataAccess/Repositories/BicycleRackRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StogoBagazines.DataAccess.Interfaces;
+using StogoBagazines.DataAccess.Objects;
+
+namespace StogoBagazines.DataAccess.Repositories
+{
+    /// <summary>
+    /// BicycleRack objects repository
+    /// </summary>
+    public class BicycleRackRepository : IRepository<BicycleRack>
+    {
+        public Database Database { get; private set; }
+        public BicycleRackRepository(Database database)
+        {
+            Database = database;
+        }
+        public object Create(BicycleRack dataObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exits(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BicycleRack Read(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<BicycleRack> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(BicycleRack updatedDataObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
