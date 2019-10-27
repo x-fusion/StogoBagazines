16ab92752f330e0512a9067bfcf87c0e9cd7f2c1
Edvardas Arnoldas Alaburda <edvardas.alaburda@ktu.edu>
2019-10-27 14:51:00 +0200

    api documentation middleware installation

---
+++ StogoBagazines/Configuration/SwaggerOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StogoBagazines.Configuration
+{
+    /// <summary>
+    /// Class which represents Swagger configuration
+    /// </summary>
+    public class SwaggerOptions
+    {
+        public string JsonRoute { get; set; }
+        public string Description { get; set; }
+        public string UIEndpoint { get; set; }
+    }
+}
--- StogoBagazines/Startup.cs
+++ StogoBagazines/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json.Converters;
 
 namespace StogoBagazines
@@ -26,8 +29,37 @@ namespace StogoBagazines
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddAuthentication(x =>
+            //{
+            //    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //}).AddJwtBearer(token =>
+            //{
+            //    token.SaveToken = true;
+            //    token.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        ValidateIssuerSigningKey = true,
+            //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetValue())
+            //    };
+            //});
             services.AddControllers();
+            services.AddMvcCore().AddApiExplorer();
             services.AddTransient(_ => new DataAccess.Database(Configuration.GetConnectionString("MySQLDb")));
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "StogoBagazines WebAPI documentation",
+                    Version = "v1",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+                    {
+                        Email = "edvardas.alaburda@ktu.edu",
+                        Name = "Edvardas-Arnoldas Alaburda",
+                        Url = new Uri("https://github.com/x-fusion")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +69,19 @@ namespace StogoBagazines
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            Configuration.SwaggerOptions swaggerOptions = new Configuration.SwaggerOptions();
+            Configuration.GetSection(nameof(StogoBagazines.Configuration.SwaggerOptions)).Bind(swaggerOptions);
+
+            app.UseSwagger(option =>
+            {
+                option.RouteTemplate = swaggerOptions.JsonRoute;
+            });
+
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint(swaggerOptions.UIEndpoint, swaggerOptions.Description);
+            });
 
             app.UseHttpsRedirection();
 
